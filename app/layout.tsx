4735f25da03a418b8f271f57d9750eb9d3a4107d
Zenquan Cai <jomsoucan@gmail.com>
2024-12-27 17:53:57 +0800

    feat

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,6 +15,13 @@ export const metadata: Metadata = {
   title: 'Zenquan',
   description: 'Zenquan 的个人站点，关于前端、JavaScript 等',
   authors: [{ name: 'Zenquan', url: 'https://blog.u-tools.fun' }],
+  viewport: {
+    width: 'device-width',
+    initialScale: 1,
+    maximumScale: 1,
+    userScalable: false,
+    viewportFit: 'cover',
+  },
   other: {
     'baidu-site-verification': '',
   },
@@ -23,22 +30,6 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="zh-CN" className={inter.variable}>
-      <head>
-        <meta 
-          name="viewport" 
-          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no, viewport-fit=cover, touch-action: manipulation" 
-        />
-        <script src="https://lf9-cdn-tos.bytecdntp.com/cdn/expire-1-M/vConsole/3.12.1/vconsole.min.js"></script>
-        <script dangerouslySetInnerHTML={{
-          __html: `
-            // 仅在非生产环境启用 vConsole
-            // if (window.location.hostname !== 'blog.u-tools.fun') {
-              var vConsole = new window.VConsole();
-              console.log('vConsole is enabled');
-            // }
-          `
-        }} />
-      </head>
       <body>
         <Menu />
         <div className="max-w-3xl mx-auto px-2">{children}</div>
